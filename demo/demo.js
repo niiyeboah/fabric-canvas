a3cd46485ac04b9cf6c2fd45bfa1a6389100a67d
Nii Yeboah <nii@vaadin.com>
2019-10-07 15:33:24 +0300

    Update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,6 @@
 import '@polymer/iron-demo-helpers/demo-pages-shared-styles';
 import '@polymer/iron-demo-helpers/demo-snippet';
-import '../src/fabric-canvas';
+import '../src/fabric-static-canvas';
+import { fabric } from '../src/fabric-canvas';
+
+window.fabric = fabric;
